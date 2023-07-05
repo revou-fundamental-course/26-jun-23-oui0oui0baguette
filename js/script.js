f25f53109b5d7061b113a9db3a8477bac3035f6c
norinorinori_3 <phileo0221@gmail.com>
2023-07-05 14:57:27 +0700

    Second-Commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+
+const slider = document.querySelector('.slider');
+const slides = slider.querySelectorAll('li');
+  
+// Store the total number of images
+const slideCount = slides.length;
+let activeSlide = 0;
+  
+setInterval(() => {
+    slides[activeSlide].classList.remove('active');
+    activeSlide++;
+    if (activeSlide === slideCount) {
+        activeSlide = 0;
+    }
+    slides[activeSlide].classList.add('active');
+}, 2000);
+
+
+//sdsdsdsd
+
 function validateForm() {
     var name = document.forms['FormText']['name'].value;
     var emailaddress = document.forms['FormText']['emailaddress'].value;
